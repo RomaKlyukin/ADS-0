4dd8d17ced3eeb3fe3eb044ea9f1a99baa268d1f
RomaKlyukin <125215143+RomaKlyukin@users.noreply.github.com>
2023-02-25 22:02:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,27 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b){
-    int x, y, sc, r, gcd;
-	if(a<0) a=-a;
-	if(b<0) b=-b;
-	if(a>b){
-		x=a;
-		y=b;
-	}
-	else{
-		x=b;
-		y=a;
-	}
-
-	do{
-		sc = 1;
-		while(y < (x - sc * y)){
-			sc += 1;
-		}
-		r = x - (sc * y);
-		x = y;
-		gcd = y;
-		y = r;
-	}
-	while(r!=0);
-	return gcd;
+int gcd(int a, int b) {
+ int x, y, sc, r, gcd;
+ if (a < 0) 
+  a = -a;
+ if (b < 0)
+  b = -b;
+ if (a > b) {
+  x = a;
+  y = b;
+ } else {
+  x = b;
+  y = a;
+ }
+ do {
+  sc = 1;
+  while (y < (x - sc * y)) {
+   sc += 1;
+  }
+  r = x - (sc * y);
+  x = y;
+  gcd = y;
+  y = r;
+ } while (r != 0);
+ return gcd;
 }
